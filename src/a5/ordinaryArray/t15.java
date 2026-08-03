7cd57286fc64cbb2b7c9fe157fadd5f11c06bc98
lxylq7 <lvxinye27@gmail.com>
2026-08-04 02:03:21 +0800

    13-17

---
+++ src/a5/ordinaryArray/t15.java
@@ -0,0 +1,34 @@
+package a5.ordinaryArray;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//轮转数组
+public class t15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
+        int[] nums = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums[i] = scanner.nextInt();
+        }
+        rotate(nums,k);
+        System.out.println(Arrays.toString(nums));
+    }
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        reverse(nums,0,k);
+        reverse(nums,k+14,nums.length-1);
+        reverse(nums,0,nums.length-1);
+    }
+    public static void reverse(int[] nums,int left,int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
