3cce0a51077304d4cc3edf55ab1c2c5b660c14f3
lxylq7 <lvxinye27@gmail.com>
2026-08-18 23:33:42 +0800

    0

--- src/a17/technique/t97.java
+++ src/a17/technique/t97.java
@@ -1,4 +1,17 @@
 package a17.technique;
 
 public class t97 {
+    public int majorityElement(int[] nums) {
+        int ans = 0;
+        int hp = 0;
+        for (int x : nums) {
+            if (hp == 0) {
+                ans = x;
+                hp = 1;
+            } else {
+                hp += x == ans ? 1 : -1;
+            }
+        }
+        return ans;
+    }
 }
--- src/a17/technique/t98.java
+++ src/a17/technique/t98.java
@@ -1,4 +1,13 @@
 package a17.technique;
 
 public class t98 {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int p0 = 0,p1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) {
+//
+            }
+        }
+    }
 }
